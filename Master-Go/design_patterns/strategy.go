6dfd7c75d101b60395ebbd6dc12406a9997a7090
AldairCoronel <aldaircr@ciencias.unam.mx>
2022-09-13 19:59:10 -0500

    feat: Add strategy design pattern

---
+++ Master-Go/design_patterns/strategy.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type PasswordProtector struct {
+	user          string
+	passwordName  string
+	hashAlgorithm HashAlgorithm
+}
+
+type HashAlgorithm interface {
+	Hash(p *PasswordProtector)
+}
+
+func NewPasswordProtector(user string, passwordName string, hash HashAlgorithm) *PasswordProtector {
+	return &PasswordProtector{
+		user:          user,
+		passwordName:  passwordName,
+		hashAlgorithm: hash,
+	}
+}
+
+func (p *PasswordProtector) SetHashAlgorith(hash HashAlgorithm) {
+	p.hashAlgorithm = hash
+}
+
+func (p *PasswordProtector) Hash() {
+	p.hashAlgorithm.Hash(p)
+}
+
+// hash algorithms
+
+type SHA struct{}
+
+func (SHA) Hash(p *PasswordProtector) {
+	fmt.Printf("Hashing using SHA for %s\n", p.passwordName)
+}
+
+type MD5 struct{}
+
+func (MD5) Hash(p *PasswordProtector) {
+	fmt.Printf("Hashing using MD5 for %s\n", p.passwordName)
+}
+
+func main() {
+	sha := &SHA{}
+	md5 := &MD5{}
+
+	passwordProtector := NewPasswordProtector("Aldair", "gmail password", sha)
+	passwordProtector.Hash()
+
+	passwordProtector.SetHashAlgorith(md5)
+	passwordProtector.Hash()
+}
